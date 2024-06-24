63d52af4591e46ddd17bb4487a19237699225d09
bread <163193066+b-bread1@users.noreply.github.com>
2024-06-24 00:43:06 -0700

    wgcf

--- misc/wgcf.ps1
+++ misc/wgcf.ps1
@@ -1,5 +1,10 @@
-mkdir "$env:USERPROFILE\Documents\wgcf\" *>$null
-cd "$env:USERPROFILE\Documents\wgcf\"
+$folderPath = "$env:USERPROFILE\Documents\wgcf\"
+if (Test-Path -Path $folderPath -PathType Container) {
+    Write-Output "Folder already exists. Continuing"
+} else {
+    mkdir $folderPath *>$null
+}
+cd $folderPath
 Invoke-RestMethod 'https://api.github.com/repos/ViRb3/wgcf/releases/latest' | % assets | ? name -like "*_windows_amd64.exe" | % { Invoke-WebRequest $_.browser_download_url -OutFile wgcf.exe }
 Start-Process -FilePath .\wgcf.exe register -Wait -NoNewWindow
 Start-Process -FilePath .\wgcf.exe generate -Wait -NoNewWindow
